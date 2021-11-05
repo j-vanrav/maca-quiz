4472ca8720c674bc67af899748c0e0d9786289c1
j-vanrav <j-vanrav@users.noreply.github.com>
2021-11-05 16:22:32 +0800

    revert to prod env

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-ENV = 'dev'
+ENV = 'prod'
 class Config(object):
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_secret'
